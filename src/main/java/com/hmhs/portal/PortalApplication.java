8b55986a979ec0c4aee65fc684d886a2391e627a
Yogesh Sharma <yogesh@Yogeshs-MacBook-Pro.local>
2020-05-18 22:46:51 -0400

    second commit

--- src/main/java/com/hmhs/portal/PortalApplication.java
+++ src/main/java/com/hmhs/portal/PortalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages = "com.hmhs.portal")
+@EnableResourceServer
 public class PortalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hmhs/portal/resource/PortalResource.java
+++ src/main/java/com/hmhs/portal/resource/PortalResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,20 @@ public class PortalResource {
 	@Autowired
 	private PortalService portalService;
 	
-	@GetMapping(value = "getIdCard/{umi}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "idcard/{umi}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<IdCardDTO> getIdCardMetadata(@PathVariable String umi){
 		logger.info("inside PortalResource getIdCardMetadata");
 		return new ResponseEntity<>(portalService.getIdCard(umi), HttpStatus.OK);
+	}
+	
+	/**
+	 * Sample delete method dosen't do anything
+	 * @param umi
+	 * @return
+	 */
+	@DeleteMapping(value = "idcard/{umi}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> deleteIdCardMetadata(@PathVariable String umi){
+		logger.info("inside PortalResource getIdCardMetadata");
+		return new ResponseEntity<>("This is test method and does not delete anything.", HttpStatus.GONE);
 	}
 }
